361ac37c27020fb4da69edaceef2daa0648165c5
Giorgio Modoni <modogio@gmail.com>
2015-02-17 10:30:56 +0100

    add winston logger

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger = require("./lib/logger");
 var bodyParser = require('body-parser');
 var app = express();
 var config = require("config");
 var routing = require('json-routing'); // set routing
 var errors = require('./lib/errors'); // mange http errors 404,500
 var  fs = require('fs');
-var path = require('path');
-
 
 
 // SET
@@ -18,7 +16,9 @@ app.set('view engine', 'jade');
 app.set('config',config);
 
 //USE
-app.use(logger('dev'));
+app.use(require('morgan')("combined",{"stream": logger.stream }));
+GLOBAL.logger = logger;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,10 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //config
 routing(app, config.get('path')); // init routing
-
-
-
-
 
 var watch = require('./lib/watchDir')(app);
 watch.start();
@@ -50,15 +46,6 @@ fs.watch('./received', function (event, filename) {
     }
 });
 */
-
-
-
-
-
-
-
-
-
 
 errors.init(app); // manage error4 404,500 etc
 
